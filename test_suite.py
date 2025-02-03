f8fc9a1ce7e1411bb5324b81d98a1a075f86c4b2
benjaminzwhite <benjaminzwhite@gmail.com>
2025-02-04 00:21:51 +0100

    feat: updated tests for sklearn

---
+++ test_suite.py
@@ -0,0 +1,43 @@
+import pytest
+
+# Basic dummy test to make sure defining functions works OK
+def my_add(x, y):
+    return x + y
+
+def test_add():
+    assert my_add(2,2) == 4
+    assert my_add(0,111) == 111
+
+
+# Project related test:
+# Here I test that scikit learn loads OK
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+
+def test_load_iris_dataset():
+    """
+    Test sklearn is working by loading a dataset
+    """
+    iris = load_iris()
+    assert iris is not None
+    assert len(iris.feature_names) == 4
+
+
+def test_logistic_regression():
+    """
+    Test that sklearn models train / fit ok
+    """
+    iris = load_iris()
+    X,y = iris.data, iris.target
+
+    model = LogisticRegression(max_iter=250) # just for testing
+    model.fit(X,y)
+
+    # test predicting works OK
+    predictions = model.predict(X)
+    assert len(predictions) == len(y)
+
+    # test predicting on a sample
+    sample = X[0].reshape(1,-1)
+    sample_pred = model.predict(sample)
+    assert sample_pred in [0,1,2] # class labels are 0,1,2
